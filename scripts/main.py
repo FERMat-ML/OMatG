9b4b7ff33b277f62b248a15952622dd14ac67ea6
Eric Fuemmeler <efuemmel@.umn.edu>
2024-09-25 16:01:28 -0500

    added mains script

---
+++ scripts/main.py
@@ -0,0 +1,10 @@
+from lightning.pytorch.cli import LightningCLI
+from omg.omg import OMG
+from ?data stuff here?
+
+def main():
+    cli = LightningCLI(OMG, ?Data?)
+
+if __name__ == "__main__":
+    main()
+
