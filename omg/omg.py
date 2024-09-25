71c3f263bcfc5967cad2d957a402f108dc04eb8f
Eric Fuemmeler <efuemmel@.umn.edu>
2024-09-25 13:02:49 -0500

    Modified main omg

--- omg/omg.py
+++ omg/omg.py
@@ -1,37 +1,71 @@
-class OMG(pl.LightningModule):
+from omg.si.stochastic_interpolants import StochasticInterpolants
+import lightning as L
+import torch
+from typing import Sequence
 
-    def __init__(self, si, sampler, model):
+class OMG(pl.LightningModule):
+    """
+    Main module on which we call Trainer.fit and generate structures via generate function.
+    """
+    
+    # TODO: specify argument types
+    def __init__(self, si: StochasticInterpolants, sampler, model):
         self.si = si 
         self.sampler = sampler
         self.model = model
 
-    def forward(self, x, t)
-        # preprocessing stuff
+    def forward(self, x_t: Sequence[torch.Tensor], t: torch.Tensor) -> Sequence[Sequence[torch.Tensor]]:
+        """
+        Calls encoder + head stack
+
+        :param x_t:
+            Sequence of torch.tensors corresponding to batched species, fractional coordinates and lattices.
+        :type x_t: Sequence[torch.Tensor]
+
+        :param t:
+            Sampled times
+        :type t: torch.Tensor
+
+        :return:
+            Predicted b and etas for species, coordinates and lattices, respectively.
+        :rtype: Sequence[Sequence[torch.Tensor]]
+        """
         x = self.model(x, t)
         return x
 
-    def training_step(self, x_1)
+    # TODO: specify argument types, determine what we want to return
+    def training_step(self, x_1):
+        """
+        Performs one training step given a batch of x_1
+        """
+
         x_0 = self.sampler.sample_p_0() # this might need x_1 as input so number of atoms are consistent 
         t = self.sample_t() # maybe just directly call torch function 
-        x_t = self.si.interpolate(x_1, x_0, t)
-        model_prediction = self.model.predict(x_t, t)
-        
-        loss = self.si.loss(model_prediction, t, x_0, x_1)
-
-        ground_truth = self.si.compute_ground_truth(x_1, x_0, t)
+        x_t = self.si.interpolate(t, x_0, x_1)
         pred = self.model(x_t, t)
         # record loss
-        loss = mse.loss(pred, ground_truth) * self.si.derivative(...) # or self.si.loss(x, ground_truth)
+        loss = si.loss(pred, t, x_0, x_1) 
        
         return loss
 
-    # def validation_step(self, batch)
+    def validation_step(self, x_1)
+        """
+        Performs one validation step given a batch of x_1
+        """
 
-    def sample(self)
-        # sample number of atoms beforehand 
+        x_0 = self.sampler.sample_p_0() # this might need x_1 as input so number of atoms are consistent 
+        t = self.sample_t() # maybe just directly call torch function 
+        x_t = self.si.interpolate(t, x_0, x_1)
+        pred = self.model(x_t, t)
+        # record loss
+        loss = si.loss(pred, t, x_0, x_1) 
+
+        return loss
+
+    def generate(self)
         x_0 = self.sampler.sample()
-        return self.si.integrate(x_0, self.model.de_fn)
-
+        gen = self.si.integrate(x_0, self.model)
+        return gen
 
 
 
