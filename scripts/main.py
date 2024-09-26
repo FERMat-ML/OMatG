8cc31ef8790aaf43ea7f7306e4b0785b6360de74
Eric Fuemmeler <efuemmel@.umn.edu>
2024-09-26 15:19:18 -0500

    added

--- scripts/main.py
+++ scripts/main.py
@@ -1,9 +1,11 @@
 from lightning.pytorch.cli import LightningCLI
-from omg.omg import OMG
-from ?data stuff here?
+#from omg.omg import OMG
+#from ?data stuff here?
+from omg.model.model import Model 
 
 def main():
-    cli = LightningCLI(OMG, ?Data?)
+    #cli = LightningCLI(OMG, ?Data?)
+    cli = LightningCLI(Model, run=False)
 
 if __name__ == "__main__":
     main()
