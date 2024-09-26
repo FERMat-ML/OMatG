a035cc1c9f727b3036a648fedb01154226600607
Philipp Hoellmer <philipp.hoellmer@gmx.de>
2024-09-26 11:47:39 -0500

    Restored changes in docstrings

--- omg/si/discrete_flow.py
+++ omg/si/discrete_flow.py
@@ -32,7 +32,9 @@ class DiscreteFlowMatchingMask(StochasticInterpolant):
             raise ValueError("Number of integration steps must be greater than 0.")
         if noise < 0.0:
             raise ValueError("Noise parameter must be greater than or equal to 0.")
-        self.S = MAX_ATOM_NUM-1
+        # self.S = MAX_ATOM_NUM
+        self.S = MAX_ATOM_NUM - 1
+        self._mask_index = MAX_ATOM_NUM
         self._number_integration_steps = number_integration_steps
         self._noise = noise
 
@@ -66,43 +68,72 @@ class DiscreteFlowMatchingMask(StochasticInterpolant):
         return x_t
 
     def loss(self, model_prediction: tuple[torch.Tensor, torch.Tensor], t: torch.Tensor, x_0: torch.Tensor,
-             x_1: torch.Tensor, n_atoms: torch.Tensor) -> torch.Tensor:
+             x_1: torch.Tensor, n_atoms: torch.Tensor, z: torch.tensor) -> torch.Tensor:
         """
-        Cross entropy loss function for discrete flow matching.
+        Compute the cross-entropy loss for the discrete flow matching between points x_0 and x_1 from two distributions
+        p_0 and p_1 at times t based on the model prediction for the probability distributions over the species.
 
-        # TODO: ASK TOM WHAT THE MODEL_PREDICTION SHOULD BE?
+        In contrast to the other stochastic interpolants in this module, the model prediction is not the velocity fields
+        b and denoisers eta. Instead, only one model prediction is required. The second model prediction is ignored
+        in this class.
+
+        Given that x_0 is of shape (sum(n_atoms),) containing the species of every atom in the batch, the model
+        prediction returns a tensor of shape (sum(n_atoms), MAX_ATOM_NUM + 1) containing the probability distribution
+        over the species (including the mask) of every atom in the batch.
 
         :param model_prediction:
-            Predicted final composition.
+            Model prediction for the probablity distributions over the species.
         :type model_prediction: tuple[torch.Tensor, torch.Tensor]
-        :param t:  
-            Times in [0,1]
+        :param t:
+            Times in [0,1].
         :type t: torch.Tensor
-        :param x_0: 
+        :param x_0:
             Points from p_0.
         :type x_0: torch.Tensor
         :param x_1:
             Points from p_1.
         :type x_1: torch.Tensor
+        :param n_atoms:
+            Number of atoms in each crystal from batch.
+        :type n_atoms: torch.Tensor
+        :param z:
+            Random variable.
+        :type z: torch.Tensor
 
         :return:
-            Loss.
+            Cross-entropy loss.
         :rtype: torch.Tensor
         """
-        return functional.cross_entropy(model_prediction, x_1)
+        assert x_0.shape == x_1.shape
+        assert model_prediction[0].shape == (x_0.shape[0], MAX_ATOM_NUM + 1)
+        return functional.cross_entropy(model_prediction[0], x_1)
 
     def integrate(self, model_function: Callable[[torch.Tensor, torch.Tensor], tuple[torch.Tensor, torch.Tensor]],
                   x_t: torch.Tensor, tspan: tuple[float, float]) -> torch.Tensor:
         """
-        Integrate the current positions x_t from time tspan[0] to tspan[1] based on the predicted x_1.
-        Implementation is based on https://arxiv.org/pdf/2402.04997. We construct rate matrix R by:
-            R(i,j|x_1) = ReLU((dp(j|x_1)/dt) - (dp(x_t|x_1)) / (S * p(x_t|x_1))
-        and add noise via "detailed balance" rate matrices R_db (see appendix):
-            R_db(i,j|x_1) = eta * delta(i,x_1) + eta * delta(j, x_1) * (St + 1 - t) / (1 - t) 
+        Integrate the current positions x_t from time tspan[0] to tspan[1] based on the probability distributions over
+        the species.
+
+        In contrast to the other stochastic interpolants in this module, the model prediction is not the velocity fields
+        b and denoisers eta. Instead, only one model prediction is required. The second model prediction is ignored
+        in this class.
+
+        Given that x_0 is of shape (sum(n_atoms),) containing the species of every atom in the batch, the model
+        prediction returns a tensor of shape (sum(n_atoms), MAX_ATOM_NUM + 1) containing the probability distribution
+        over the species (including the mask) of every atom in the batch.
+
+        Following https://arxiv.org/pdf/2402.04997, we construct a rate matrix R by
+        R(i,j|x_1) = ReLU((dp(j|x_1)/dt) - (dp(x_t|x_1)) / (S * p(x_t|x_1)),
+        and add noise via "detailed balance" rate matrices R_db (see appendix)
+        R_db(i,j|x_1) = eta * delta(i,x_1) + eta * delta(j, x_1) * (St + 1 - t) / (1 - t).
+
+        In addition, we discretize time instead of using a continuous time Markov process.
+
+        TODO: I think we should implement a continuous-time Markov process in the future.
 
         :param model_function:
-            Model function returning the velocity fields b and the denoisers eta given the current times t and positions
-            x_t.
+            Model function returning the probability distributions over the species given the current times t and
+            positions x_t.
         :type model_function: Callable[[torch.Tensor, torch.Tensor], tuple[torch.Tensor, torch.Tensor]]
         :param x_t:
             Current positions.
