f11bef9a36361db14ae7d974d0e7400158b164f9
Eric Fuemmeler <efuemmel@.umn.edu>
2024-09-27 15:05:51 -0500

    cleaned utils

--- omg/model/model_utils.py
+++ omg/model/model_utils.py
@@ -18,20 +18,3 @@ class SinusoidalTimeEmbeddings(nn.Module):
         embeddings = torch.cat((embeddings.sin(), embeddings.cos()), dim=-1)
         return embeddings
 
-# TODO: move to overall utils and make options accesible to OMG via CLI
-def xyz_saver(data: Data):
-    """
-    Takes data that has been generated and saves it as xyz file
-    """
-    from ase import Atoms
-    from ase.io import write
-    import time
-    batch_size = len(data.n_atoms)
-    atoms = []
-    for i in range(batch_size):
-        lower, upper = data.ptr[i*1], data.ptr[(i*1)+1]
-        atoms.append(Atoms(numbers=data.species[lower:upper], scaled_positions =data.pos[lower:upper, :], cell=data.cell[i, :, :]))
-    write(f'{time.strftime("%Y%m%d-%H%M%S")}.xyz', atoms)
-
-    
-
