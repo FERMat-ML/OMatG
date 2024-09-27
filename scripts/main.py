624ad18ebe429f40b3bb6c3501c8d85bf771917c
Eric Fuemmeler <efuemmel@.umn.edu>
2024-09-26 20:54:09 -0500

    runnable

--- scripts/main.py
+++ scripts/main.py
@@ -3,7 +3,7 @@ from omg.omg import OMG
 from omg.datamodule.dataloader import OMGDataModule
 
 def main():
-    cli = LightningCLI(OMG,OMGDataModule, run=False)
+    cli = LightningCLI(OMG,OMGDataModule)
 
 if __name__ == "__main__":
     main()
