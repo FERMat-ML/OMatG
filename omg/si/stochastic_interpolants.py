0887a6576c1ae2bc0b1fd0ca793c0d05f80e8eaa
thomas-egg <tje3676@nyu.edu>
2024-09-30 19:00:18 -0400

    Bugfix

--- omg/si/stochastic_interpolants.py
+++ omg/si/stochastic_interpolants.py
@@ -219,8 +219,7 @@ class StochasticInterpolants(object):
                     b, eta = b.reshape((-1,)), eta.reshape((-1,))
                     return b, eta
 
-                # Should this be x_int_dict? Doesn't seem that x_t_dict gets updated beyond initial creation 
-                new_x_t_dict[data_field.name].copy_(stochastic_interpolant.integrate(model_prediction_fn,
+                x_int_dict[data_field.name].copy_(stochastic_interpolant.integrate(model_prediction_fn,
                                                     x_t_dict[data_field.name], tspan))
-            x_t = new_x_t.clone(*[data_field.name for data_field in self._data_fields])
+            x_t = x_int_dict.clone(*[data_field.name for data_field in self._data_fields])
         return x_t
