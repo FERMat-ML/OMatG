9777b6fa71c279c7b88d92f53ac2dc78e1aed000
thomas-egg <tje3676@nyu.edu>
2024-10-03 12:47:47 -0400

    Fix suboptimal integration step

--- omg/si/stochastic_interpolants.py
+++ omg/si/stochastic_interpolants.py
@@ -221,9 +221,9 @@ class StochasticInterpolants(object):
                     b, eta = b.reshape((-1,)), eta.reshape((-1,))
                     return b, eta
                     
-                x_int_dict[data_field.name].copy_(stochastic_interpolant.integrate(model_prediction_fn,
-                                                    x_t_dict[data_field.name], tspan))
-                x_t[data_field.name] = x_int_dict[data_field.name]
+                new_x_t_dict[data_field.name].copy_(stochastic_interpolant.integrate(model_prediction_fn,
+                                                    x_int_dict[data_field.name], tspan))
+                x_t[data_field.name] = new_x_t_dict[data_field.name]
             x_t_dict = x_t.to_dict()
             if save_intermediate:
                 inter_list.append(x_t)
