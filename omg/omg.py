cc5d5d9f2f48cb762ffd692a76f153ca49af5f21
thomas-egg <tje3676@nyu.edu>
2024-10-22 09:47:53 -0400

    put on device

--- omg/omg.py
+++ omg/omg.py
@@ -128,7 +128,7 @@ class OMG(L.LightningModule):
         """
         Performs generation
         """
-        x_0 = self.sampler.sample_p_0()
+        x_0 = self.sampler.sample_p_0().to(self.device)
         gen, inter = self.si.integrate(x_0, self.model, save_intermediate=True)
         # probably want to turn structure back into some other object that's easier to work with
         xyz_saver(gen)
