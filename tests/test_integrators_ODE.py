f108bf1edf0462fd6ff5a4474d98631ddddc280a
thomas-egg <tje3676@nyu.edu>
2024-10-23 14:38:23 -0400

    Add testing changes

---
+++ tests/test_integrators_ODE.py
@@ -0,0 +1,78 @@
+import pytest
+import torch
+from omg.si.single_stochastic_interpolant import SingleStochasticInterpolant
+from omg.si.corrector import PeriodicBoundaryConditionsCorrector
+from omg.si.interpolants import *
+from omg.si.gamma import *
+from omg.si.epsilon import *
+from omg.globals import SMALL_TIME, BIG_TIME
+
+# Testing parameters/objects
+tol = 1e-2
+ptr = None
+eps = 1e-3
+times = torch.linspace(SMALL_TIME, BIG_TIME, 100)
+
+# Interpolants
+interpolants = [
+    LinearInterpolant(),
+    TrigonometricInterpolant(),
+    PeriodicLinearInterpolant(),
+    EncoderDecoderInterpolant(),
+    MirrorInterpolant(),
+    ScoreBasedDiffusionModelInterpolant()
+]
+
+# Interpolant arguments
+gammas = [
+    None
+]
+
+def get_name(obj):
+    return obj.__class__.__name__ if obj is not None else "None"
+
+@pytest.mark.parametrize(
+    "gamma, interpolant", 
+    [(gamma, interpolant) for gamma in gammas for interpolant in interpolants],
+    ids=[
+        f"gamma={get_name(gamma)}, interpolant={get_name(interpolant)}"
+        for gamma in gammas for interpolant in interpolants
+    ]
+)
+def test_integrator(interpolant, gamma):
+    '''
+    Test interpolant integrator
+    '''
+    # Initialize
+    x_init = torch.rand(size=(4,))
+    x_final = torch.rand(size=(4,))
+    corr = None
+    if isinstance(interpolant, PeriodicLinearInterpolant):
+        corr = PeriodicBoundaryConditionsCorrector(min_value=0, max_value=1)
+    if isinstance(interpolant, MirrorInterpolant):
+        x_init = x_final
+
+    # Design interpolant
+    interpolant = SingleStochasticInterpolant(
+        interpolant=interpolant, gamma=gamma,epsilon=None,
+        differential_equation_type='ODE', corrector=corr,
+        integrator_kwargs={'method':'rk4'}
+    )
+
+    # ODE function
+    velo = lambda t, x : interpolant._interpolate_derivative(torch.tensor(t), x_init, x_final, z=0, batch_pointer=None)
+    
+    # Integrate
+    x = x_init
+    for i in range(1,len(times)):
+        t_i = times[i-1]
+        dt = times[i] - t_i
+        x_interp = interpolant.interpolate(times[i], x_init, x_final, ptr)[0]
+        x_new = interpolant._ode_integrate(velo, x, t_i, dt)
+        x = x_new
+        #assert x == pytest.approx(x_interp, abs=tol)
+
+    # Test for equality
+    print(x)
+    print(x_final - x)
+    assert x == pytest.approx(x_final, abs=tol)
