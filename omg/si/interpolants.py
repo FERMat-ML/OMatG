648592fbc6173a4829ed817c4704344fc49f2d9d
Maya Martirossyan <mm9084@log-1.hpc.nyu.edu>
2024-10-24 11:07:57 -0400

    first commit of periodic SBDM interpolant

--- omg/si/interpolants.py
+++ omg/si/interpolants.py
@@ -138,7 +138,7 @@ class TrigonometricInterpolant(Interpolant):
 class PeriodicLinearInterpolant(Interpolant):
     """
     Linear interpolant I(t, x_0, x_1) = exp_(x_0)(t * log_(x_0))(x_1)) (see Eqs (11)-(13) in
-    https://arxiv.org/pdf/2302.03660) between points x_0 and x_1 from two distributions p_0 and p_1 at times t on a
+    https://arxiv.org/pdf/2406.04713) between points x_0 and x_1 from two distributions p_0 and p_1 at times t on a
     periodic manifold.
 
     The exponential and logarithmic maps are given by:
@@ -419,4 +419,82 @@ class ScoreBasedDiffusionModelInterpolant(Interpolant):
         :rtype: torch.Tensor
         """
         assert self._check_t(t)
-        return -t / torch.sqrt(1.0 - (t ** 2)) * x_0 + x_1
+        return -t / torch.sqrt(1.0 - (t ** 2)) * x_0 + x_1 # TODO: is this right?
+
+class PeriodicScoreBasedDifussionModelInterpolant(Interpolant):
+    
+    def __init__(self) -> None:
+        """
+        Construct VP interpolant
+        """
+        super().__init__()
+
+    def interpolate(self, t: torch.Tensor, x_0: torch.Tensor, x_1: torch.Tensor,
+                    batch_pointer: torch.Tensor) -> torch.Tensor:
+        """
+        Interpolate between points x_0 and x_1 from two distributions p_0 and p_1 at times t on a torus.
+
+        :param t:
+            Times in [0,1].
+        :type t: torch.Tensor
+        :param x_0:
+            Points from p_0.
+        :type x_0: torch.Tensor
+        :param x_1:
+            Points from p_1.
+        :type x_1: torch.Tensor
+        :param batch_pointer:
+            Tensor of length batch_size + 1 containing the indices to the first atom in every batch plus the total
+            number of atoms in the batch.
+        :type batch_pointer: torch.Tensor
+
+        :return:
+            Interpolated value.
+        :rtype: torch.Tensor
+        """
+        assert self._check_t(t)
+        # omega = 2.0 * torch.pi * (x_1 - x_0)
+        # out = t * torch.atan2(torch.sin(omega), torch.cos(omega)) / (2.0 * torch.pi)
+        # return out + x_0 - torch.floor(out + x_0)
+        x_min = 0. # TODO: input?
+        x_max = 1. # TODO: input?
+        x_mid = (x_max - x_min)/2
+        diff = torch.abs(x_0-x_1)
+        x_1prime = torch.where(diff >= x_mid, x_1 + torch.sign(x_0-x_mid), x_1)
+        path = torch.sqrt(1.0 - (t ** 2))*x_0.reshape(1, -1) + t * x_1prime.reshape(1, -1) # TODO: check reshape 
+
+        return path%(x_max-x_min)
+
+    def interpolate_derivative(self, t: torch.Tensor, x_0: torch.Tensor, x_1: torch.Tensor,
+                               batch_pointer: torch.Tensor) -> torch.Tensor:
+        """
+        Compute the derivative of the interpolant between points x_0 and x_1 from two distributions p_0 and p_1 at times
+        t with respect to time.
+
+        :param t:
+            Times in [0,1].
+        :type t: torch.Tensor
+        :param x_0:
+            Points from p_0.
+        :type x_0: torch.Tensor
+        :param x_1:
+            Points from p_1.
+        :type x_1: torch.Tensor
+        :param batch_pointer:
+            Tensor of length batch_size + 1 containing the indices to the first atom in every batch plus the total
+            number of atoms in the batch.
+        :type batch_pointer: torch.Tensor
+
+        :return:
+            Derivative of the interpolant.
+        :rtype: torch.Tensor
+        """
+        assert self._check_t(t)
+        x_min = 0. # TODO: input?
+        x_max = 1. # TODO: input?
+        x_mid = (x_max - x_min)/2
+        diff = torch.abs(x_0-x_1)
+        x_1prime = torch.where(diff >= x_mid, x_1 + torch.sign(x_0-x_mid), x_1)
+        der = -t / torch.sqrt(1.0 - (t ** 2))*x_0.reshape(1, -1) + x_1prime.reshape(1, -1) # TODO: check reshape
+
+        return der
