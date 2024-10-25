081fb6b0d1ae2fb8a66caa181d7f0f525bb2dcfc
Philipp Hoellmer <philipp.hoellmer@gmx.de>
2024-10-25 16:36:29 -0400

    Updated test code to two elements in batch

--- example/translation_test_periodic_interpolant.py
+++ example/translation_test_periodic_interpolant.py
@@ -4,14 +4,18 @@ from omg.si.interpolants import PeriodicLinearInterpolant
 
 def main():
     interpolant = PeriodicLinearInterpolant()
-    n_atoms = 4  # Changing this to 10 messes most of the values up.
+    n_atoms = 2  # Changing this to 10 messes most of the values up.
     dims = 3
-    batch_pointer = torch.tensor([0, n_atoms])
-    times = torch.full((n_atoms, dims), 0.3)
+    batch_pointer = torch.tensor([0, n_atoms, 2 * n_atoms])
+    times = torch.zeros((2 * n_atoms, dims))
+    times[:n_atoms, :] = 0.3
+    times[n_atoms:, :] = 0.7
 
-    x_0 = torch.rand((n_atoms, dims))
-    x_1 = torch.rand((n_atoms, dims))
-    shift = (torch.rand(dims) - 0.5).repeat(n_atoms, 1)
+    x_0 = torch.rand((2 * n_atoms, dims))
+    x_1 = torch.rand((2 * n_atoms, dims))
+    shift = torch.zeros_like(x_0)
+    shift[:n_atoms] = (torch.rand(dims) - 0.5).repeat(n_atoms, 1)
+    shift[n_atoms:] = (torch.rand(dims) - 0.5).repeat(n_atoms, 1)
 
     shifted_x_0 = (x_0 + shift) % 1.0
     shifted_x_1 = (x_1 + shift) % 1.0
