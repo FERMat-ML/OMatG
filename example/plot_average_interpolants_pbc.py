fixed averaging x_ref

example/plot_average_interpolants_pbc.py
@@ -6,13 +6,12 @@ from omg.si.interpolants import PeriodicLinearInterpolant
 from omg.si.single_stochastic_interpolant import SingleStochasticInterpolant
 
 
-tries = 10000
+tries = 1000
 time_steps = 1000
 
 
-def pbc_mean(x):
+def pbc_mean(x, x_ref):
     # assuming pbcs from 0 to 1
-    x_ref = x[0]
     dists = np.abs(x - x_ref) 
     x_prime = np.where(dists >=0.5, x + np.sign(x_ref - 0.5), x)
     return np.average(x_prime) % 1.
@@ -26,7 +25,7 @@ def main():
         differential_equation_type="ODE")
 
     x_0 = torch.tensor([[0.1, 0.2]])
-    x_1 = torch.tensor([[0.9, 0.2]])
+    x_1 = torch.tensor([[0.9, 0.8]])
     diff = torch.abs(x_1 - x_0)
     x_1_prime = torch.where(diff >= 0.5, x_1 + torch.sign(x_0 - 0.5), x_1)
     batch_pointer = torch.tensor([0, 1])
@@ -59,9 +58,14 @@ def main():
         x_t_paths_linear_with_gamma.append(x_t_path_linear_with_gamma)
 
     x_t_paths_linear_with_gamma = np.array(x_t_paths_linear_with_gamma)
-    mean_x_t_path_linear_with_gamma = np.array([[pbc_mean(x_t_paths_linear_with_gamma[:, i, 0]), pbc_mean(x_t_paths_linear_with_gamma[:, i, 1])] for i in range(x_t_paths_linear_with_gamma.shape[1])])
+    #x_ref = x_t_path_linear_without_gamma
+    x_ref = np.zeros_like(x_t_path_linear_without_gamma)
+    mean_x_t_path_linear_with_gamma = np.array( \
+        [[pbc_mean(x_t_paths_linear_with_gamma[:, i, 0], x_ref[i, 0]), 
+        pbc_mean(x_t_paths_linear_with_gamma[:, i, 1], x_ref[i, 1])] \
+        for i in range(x_t_paths_linear_with_gamma.shape[1])])
 
-    plt.scatter(mean_x_t_path_linear_with_gamma[:, 0], mean_x_t_path_linear_with_gamma[:, 1], color="C1", marker=".")
+    plt.scatter(mean_x_t_path_linear_with_gamma[:, 0], mean_x_t_path_linear_with_gamma[:, 1], color="C1", marker=".", s=0.5)
 
 
     plt.axhline(y=0.0, color="black")