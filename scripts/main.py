ffcf50500ee521305f0935964d4d8a07fc2d2b93
thomas-egg <tje3676@nyu.edu>
2024-11-07 18:04:53 -0500

    Add new main

--- scripts/main.py
+++ scripts/main.py
@@ -1,9 +1,10 @@
-from lightning.pytorch.cli import LightningCLI
+#from lightning.pytorch.cli import LightningCLI
+from omg.omg_cli import OMG_CLI
 from omg.omg import OMG
 from omg.datamodule.dataloader import OMGDataModule
 
 def main():
-    cli = LightningCLI(OMG, OMGDataModule, save_config_kwargs={"overwrite": True})
+    cli = OMG_CLI(OMG, OMGDataModule, save_config_kwargs={"overwrite": True})
 
 if __name__ == "__main__":
     main()
