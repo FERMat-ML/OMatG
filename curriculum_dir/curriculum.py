f414d9d1c70d4409ca1d0ad2433b4c111e2cfa33
thomas-egg <tje3676@nyu.edu>
2024-11-22 10:28:28 -0500

    Add curriculum file and example lesson

---
+++ curriculum_dir/curriculum.py
@@ -0,0 +1,68 @@
+import subprocess
+import sys
+import torch
+import yaml
+import numpy as np
+from enum import Enum
+from pathlib import Path
+from omg.si.single_stochastic_interpolant_identity import SingleStochasticInterpolantIdentity
+
+class ValidLessons(Enum):
+    species = 0
+    pos = 1
+    cell = 2
+
+def write_lesson(filename:str, lessons:ValidLessons):
+    '''
+    Write a config file for a particular "lesson" in curriculum learning style
+    whereby only one interpolant will be trained
+
+    :param filename:
+        Name of file to alter
+    :type filename: str
+    :param lesson:
+        Which lesson
+    :type lesson: ValidLessons
+    '''
+
+    # Load yaml
+    with open(filename, 'r') as file:
+        template = yaml.safe_load(file)
+        file.close()
+
+    # Get interpolant and swap with identity
+    mask_path = 'omg.si.single_stochastic_interpolant_identity.SingleStochasticInterpolantIdentity'
+    interpolants = template['model']['si']['init_args']['stochastic_interpolants']
+    distributions = template['model']['sampler']['init_args']
+    costs = template['model']['relative_si_costs']
+    valid_lessons = list(ValidLessons)
+    for lesson in lessons:
+        for i in range(len(interpolants)):
+            if i == lesson.value:
+                interpolants[i] = interpolants[i]
+                costs[i] = 1 / (len(lessons))
+            else:
+                distributions[f'{valid_lessons[i].name}_distribution'] = 'omg.sampler.distributions.MirrorData'
+                interpolants[i]['class_path'] = mask_path
+                interpolants[i]['init_args'] = None
+                costs[i] = 0.0
+
+    # Save lesson
+    template['model']['si']['init_args']['stochastic_interpolants'] = interpolants
+    template['model']['relative_si_costs'] = costs
+    base, ext = filename.split(".", 1)
+    lesson_slice = ''
+    for lesson in lessons:
+        lesson_slice += f'{lesson.name}_'
+    lesson_name = f"{base}_{lesson_slice}.{ext}"
+    with open(f'lessons/{lesson_name}', 'w') as file:
+        yaml.safe_dump(template, file)
+
+if __name__ == '__main__':
+
+    # Get config file
+    filename = sys.argv[1]
+
+    # Write a lesson
+    lesson = [ValidLessons(1)]
+    write_lesson(filename, lesson)
