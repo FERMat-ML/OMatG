3639368ad5812ce123b2985b3065e25dd2f864a5
Philipp Hoellmer <philipp.hoellmer@gmx.de>
2026-01-01 21:12:52 -0500

    Fix pickling issue

--- omg_tf/rewards.py
+++ omg_tf/rewards.py
@@ -3,6 +3,7 @@ import os
 from typing import Optional, Sequence
 import numpy as np
 from pymatgen.analysis.structure_matcher import StructureMatcher
+from pymatgen.core.structure import Structure as PymatgenStructure
 import tqdm
 from tqdm.contrib.concurrent import process_map
 from omg.datamodule import OMGDataset, Structure
@@ -89,14 +90,14 @@ class CRMSEReward(Reward):
         self._cpu_count = number_cpus if number_cpus is not None else os.cpu_count()
 
     @staticmethod
-    def _compute_rmse(structure: Structure, reference_dataset: OMGDataset, ltol: float, stol: float,
+    def _compute_rmse(py_structure: PymatgenStructure, reference_dataset: OMGDataset, ltol: float, stol: float,
                       angle_tol: float) -> float:
         """
         Compute the cRMSE between a generated structure and the closest matching structure in the reference dataset.
 
-        :param structure:
-            Generated structure.
-        :type structure: Structure
+        :param py_structure:
+            Generated structure as a Pymatgen Structure.
+        :type py_structure: PymatgenStructure
         :param reference_dataset:
             Reference dataset for computing cRMSE.
         :type reference_dataset: OMGDataset
@@ -115,7 +116,6 @@ class CRMSEReward(Reward):
         :rtype: float
         """
         sm = StructureMatcher(ltol=ltol, stol=stol, angle_tol=angle_tol)
-        py_structure = structure.get_pymatgen_structure()
         py_composition = py_structure.composition.reduced_composition
         relevant_py_structures = []
         for reference_structure in reference_dataset.get_structure_dataset():
@@ -154,14 +154,17 @@ class CRMSEReward(Reward):
         """
         crmse_function = partial(self._compute_rmse, reference_dataset=reference_dataset, ltol=self._ltol,
                                  stol=self._stol, angle_tol=self._angle_tol)
+        # Be careful to convert structures to pymatgen structures before parallel processing to avoid pickling issues
+        # with torch tensors.
+        py_structures = [structure.get_pymatgen_structure() for structure in structures]
 
         if self._cpu_count > 1:
-            crmse_values = process_map(crmse_function, structures, desc="Computing cRMSE rewards",
-                                       chunksize=max(min(len(structures) // self._cpu_count, 100), 1),
+            crmse_values = process_map(crmse_function, py_structures, desc="Computing cRMSE rewards",
+                                       chunksize=max(min(len(py_structures) // self._cpu_count, 100), 1),
                                        max_workers=self._cpu_count)
         else:
-            crmse_values = list(map(crmse_function,
-                                    tqdm.tqdm(structures, desc="Computing cRMSE rewards", total=len(structures))))
+            crmse_values = list(map(crmse_function, tqdm.tqdm(py_structures, desc="Computing cRMSE rewards",
+                                                              total=len(py_structures))))
 
         return self._scale * (self._stol - np.array(crmse_values))
 
